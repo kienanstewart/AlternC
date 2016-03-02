96eda4e85e717c514e6ebeca7986aee70e1a36fb
alternc <contact@alternc.com>
2016-03-02 10:40:00 +0100

    adding new assets

--- doc/www/head.php
+++ doc/www/head.php
@@ -12,7 +12,7 @@
 
 <!-- Bootstrap core CSS -->
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="/assets/bootstrap.min.css">
 
 <link rel="stylesheet" href="/assets/css/docs.min.css">
 
@@ -21,8 +21,8 @@
 
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <script src="/assets/html5shiv.min.js"></script>
+  <script src="/assets/respond.min.js"></script>
 <![endif]-->
 
 <!-- Favicons -->
